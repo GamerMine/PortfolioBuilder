91b5c29d6bd40d1b64c4023797226b5e10c67486
sm211563 <maxime-sav@outlook.fr>
2023-04-03 15:12:55 +0200

    Fix
    - Logout is working
    - Fixed index.html

--- js/main.js
+++ js/main.js
@@ -26,74 +26,6 @@ export function requestVerifyConnection() {
 }
 
 export function showAllPortfolio() {
-    const TEST = "<!DOCTYPE html>\n" +
-        "<html lang=\"fr\">\n" +
-        "\n" +
-        "<head>\n" +
-        "\t<meta charset=\"UTF-8\">\n" +
-        "\t<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
-        "\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-        "\t<script type=\"module\" src=\"js/main.js\"></script>\n" +
-        "\t<link rel=\"stylesheet\" href=\"css/style.css\">\n" +
-        "\t<link rel=\"shortcut icon\" href=\"favicon.ico\" type=\"image/x-icon\">\n" +
-        "\t<title>Portfolio Builder</title>\n" +
-        "</head>\n" +
-        "\n" +
-        "<body>\n" +
-        "\t<header>\n" +
-        "\t\t<h1>Portfolio Builder</h1>\n" +
-        "\t\t<div id=\"btn-login\">\n" +
-        "\t\t\t<button id=\"login\" type=\"button\">Connexion</button>\n" +
-        "\t\t\t<button class=\"button\" id=\"register\" type=\"button\" onclick=\"registerForm()\">S'inscrire</button>\n" +
-        "\t\t</div>\n" +
-        "\t</header>\n" +
-        "\n" +
-        "\n" +
-        "\t<main>\n" +
-        "\t\t<h3>Liste des portfolio</h3>\n" +
-        "\t\t<section id=\"list-portfolio\">\n" +
-        "\n" +
-        "\t\t\t<article class=\"card-portfolio\">\n" +
-        "\t\t\t\t<h5>Portfolio de X</h5>\n" +
-        "\t\t\t\t<div></div>\n" +
-        "\t\t\t</article>\n" +
-        "\n" +
-        "\t\t\t<article class=\"card-portfolio\">\n" +
-        "\t\t\t\t<h5>Portfolio de X</h5>\n" +
-        "\t\t\t\t<div></div>\n" +
-        "\t\t\t</article>\n" +
-        "\n" +
-        "\t\t\t<article class=\"card-portfolio\">\n" +
-        "\t\t\t\t<h5>Portfolio de X</h5>\n" +
-        "\t\t\t\t<div></div>\n" +
-        "\t\t\t</article>\n" +
-        "\n" +
-        "\t\t\t<article class=\"card-portfolio\">\n" +
-        "\t\t\t\t<h5>Portfolio de X</h5>\n" +
-        "\t\t\t\t<div></div>\n" +
-        "\t\t\t</article>\n" +
-        "\n" +
-        "\t\t\t<article class=\"card-portfolio\">\n" +
-        "\t\t\t\t<h5>Portfolio de X</h5>\n" +
-        "\t\t\t\t<div></div>\n" +
-        "\t\t\t</article>\n" +
-        "\t\t\t\n" +
-        "\t\t</section>\n" +
-        "\t\t<div class=\"right-position\">\n" +
-        "\t\t\t<button id=\"create-portfolio\" type=\"button\" class=\"button\">Créer votre portfolio</button>\n" +
-        "\t\t</div>\n" +
-        "\t</main>\n" +
-        "\n" +
-        "\t<footer>\n" +
-        "\t\t<p>\n" +
-        "\t\t\t<img src=\"picture/Cc_by-nd_icon.svg\"/>\n" +
-        "\t\t\tCD BY-ND\n" +
-        "\t\t</p>\n" +
-        "\t\tMaxime SAVARY - Louis LE CLÉAC'H - Elie BRION - Léo LANGLOIS\n" +
-        "\t</footer>\n" +
-        "</body>\n" +
-        "\n" +
-        "</html>";
     const request = new XMLHttpRequest();
     const portfolioList = document.getElementById("list-portfolio");
 
@@ -103,36 +35,62 @@ export function showAllPortfolio() {
     request.onreadystatechange = () => {
         if (request.readyState === 4) {
             const response = JSON.parse(request.response);
-            console.log(response);
 
             for (const userHome of response.result) {
-                console.log(userHome);
+                portfolioList.appendChild(createPortfolioPreviewElement(userHome.name, userHome.surname, userHome.mail, userHome.homecontent));
             }
         }
     }
 
+}
+
+function moveToPortfolioView(mail) {
+    const request = new XMLHttpRequest();
+    request.open("GET", URL_BASE+"server/sendData.php?command=SET_LOCATION&location=homeContentIN"+mail);
+    request.send();
+
+    request.onreadystatechange = () => {
+        if (request.readyState === 4) {
+            window.location.href = "portfolio.html";
+        }
+    }
+}
+
+function createPortfolioPreviewElement(name, surname, mail, homeContent) {
     const article = document.createElement("article");
     const h5 = document.createElement("h5");
     const div = document.createElement("div");
     const embed = document.createElement("iframe");
 
     h5.onclick = () => {
-        // TODO: Load view only if current connected user is the owner
-        window.location.href = "#";
+        moveToPortfolioView(mail);
+
     }
 
     div.onclick = () => {
-        // TODO: Load view only if current connected user is the owner
-        window.location.href = "#";
+        moveToPortfolioView(mail);
     }
 
     article.classList.add("card-portfolio");
-    h5.innerText = "Test";
-    embed.src = "index.html";
+    h5.innerText = name + " " + surname;
+    embed.srcdoc = homeContent;
     embed.scrolling = "no";
+
     article.appendChild(h5);
     article.appendChild(div);
     div.appendChild(embed);
-    portfolioList.appendChild(article);
 
+    return article;
+}
+
+export function disconnect() {
+    const request = new XMLHttpRequest();
+    request.open("GET", URL_BASE+"server/disconnect.php");
+    request.send();
+
+    request.onreadystatechange = () => {
+        if (request.readyState === 4) {
+            window.location.href = "index.html";
+        }
+    }
 }
--- server/requestData.php
+++ server/requestData.php
@@ -2,13 +2,32 @@
 
 include "Database.php";
 
+ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+
 $requestedData = $_GET["command"];
 $data = array("result" => null);
 $db = Database::getInstance();
 
+session_start();
+
 switch ($requestedData) {
     case "ALL_USER_PORTFOLIO": {
         $data = array("result" => $db->getAllUserHome());
+        break;
+    }
+    case "USER_PORTFOLIO": {
+        $data = array("content" => $db->getUserHome($_GET["mail"]));
+        break;
+    }
+    case "GET_LOCATION": {
+        $data = array("result" => $_SESSION["location"]);
+        break;
+    }
+    case "GET_CONTENT": {
+        if (strtolower($_GET["name"]) == "homecontent") {
+            $data = array("content" => $db->getHomeContent($_GET["mail"]));
+        }
+        break;
     }
 }
 
