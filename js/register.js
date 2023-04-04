e8c2d2fedcdab01d2725673581d67b03b4c11159
sm211563 <maxime-sav@outlook.fr>
2023-04-04 17:35:04 +0200

    On répare les connerie de ELIE
    fait par elie sur le compte de maxime !
    Message de warn pour login et register!
    + css dans oui.css pour warn

--- js/register.js
+++ js/register.js
@@ -12,6 +12,23 @@ async function register() {
     const passField    = document.getElementById("pass");
     const verifPassField = document.getElementById("verifPass");
 
+    if(document.getElementById("warn") != null) document.getElementById("warn").remove();
+
+    const warn = document.createElement("p");
+    warn.classList.add("warn");
+    warn.id = "warn";
+
+
+    console.log(document.getElementById("pass").innerText);
+    console.log(document.getElementById("verifPass").innerText);
+
+    if(document.getElementById("pass").value !== document.getElementById("verifPass").value) {
+        console.log('ça marche');
+        warn.innerText = "Les mots de passe ne correspondent pas !";
+        document.getElementById("form").firstChild.after(warn);
+        return;
+    }
+
     await request.open("POST", URL_BASE+"server/register.php?mail="+addressField.value+"&pass="+passField.value+"&verifPass=" +verifPassField.value, true);
     await request.send();
 
@@ -24,10 +41,12 @@ async function register() {
                     console.log("Redirection");
                     window.location.href = "../index.html";
                 } else {
-                    // TODO: Wrong mail or password popup
+                    warn.innerText = "Il existe déjà un compte associé à cette adresse mail !";
+                    document.getElementById("form").firstChild.after(warn);
                 }
             } catch (e) {
-                // TODO: Server communication error popup
+                warn.innerText = "Un problème est survenu du coté serveur !";
+                document.getElementById("form").firstChild.after(warn);
                 console.log("Connexion impossible, rententez plus tard");
             }
         }
