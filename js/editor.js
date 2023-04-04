74c2d32beafc47c74d530160ca089ce4af6a7213
leo.langlois <langlois76190@gmail.com>
2023-04-04 14:10:22 +0200

    Bouton du menu tools qui se change avec "Ajouter élémenté et "Retour"

---
+++ js/editor.js
@@ -0,0 +1,163 @@
+
+
+/*
+<div>Titre</div>
+
+<button class="button" id="btn-home" type="button">Accueil</button>
+
+
+<button class="button" id="btn-project" type="button">Projet</button>
+
+
+<button class="button" id="btn-skill" type="button">Compétence</button>
+
+
+<button class="button" id="btn-a-propos" type="button">A propos</button>
+*/
+/*
+<button class="button" id="btn-add" type="button">Ajouter élément</button>
+*/
+
+
+var lblTitre   = document.createElement("div");
+var btnHome    = document.createElement("button");
+var btnProject = document.createElement("button");
+var btnSkill   = document.createElement("button");
+var btnApropos = document.createElement("button");
+
+var btnAjouter = document.createElement("button");
+
+
+lblTitre.innerHTML = "Titre";
+
+btnHome.setAttribute("class", "button");
+btnHome.setAttribute("id", "btn-home");
+btnHome.setAttribute("type", "button");
+btnHome.innerHTML = "Accueil";
+
+btnProject.setAttribute("class", "button");
+btnProject.setAttribute("id", "btn-project");
+btnProject.setAttribute("type", "button");
+btnProject.innerHTML = "Projet";
+
+btnSkill.setAttribute("class", "button");
+btnSkill.setAttribute("id", "btn-skill");
+btnSkill.setAttribute("type", "button");
+btnSkill.innerHTML = "Compétence";
+
+btnApropos.setAttribute("class", "button");
+btnApropos.setAttribute("id", "btn-a-propos");
+btnApropos.setAttribute("type", "button");
+btnApropos.innerHTML = "A propos";
+
+
+btnAjouter.setAttribute("class", "button");
+btnAjouter.setAttribute("id", "btn-add");
+btnAjouter.setAttribute("type", "button");
+
+
+function toolsBase()
+{
+    var divSelect  = document.getElementById("btnselect");
+    var divBottom  = document.getElementById("bottom");
+
+    while (divSelect.firstChild)
+    {
+        divSelect.removeChild(divSelect.firstChild);
+    }
+
+    divBottom.removeChild(divBottom.firstChild);
+
+    btnAjouter.innerHTML = "Ajouter élément";
+    
+    divSelect.appendChild(lblTitre);
+    divSelect.appendChild(btnHome);
+    divSelect.appendChild(btnProject);
+    divSelect.appendChild(btnSkill);
+    divSelect.appendChild(btnApropos);
+
+    divBottom.appendChild(btnAjouter);
+
+
+    document.getElementById("btn-add").addEventListener("click", modifTools, false);
+}
+
+/*
+<div>Titre</div>
+
+<button class="button" id="btn-txt" type="button">Accueil</button>
+
+
+<button class="button" id="btn-img" type="button">Projet</button>
+
+
+<button class="button" id="btn-link" type="button">Compétence</button>
+
+
+<button class="button" id="btn-cv" type="button">A propos</button>
+*/
+
+
+
+var lblAjout   = document.createElement("div");
+var btnTxt     = document.createElement("button");
+var btnImg     = document.createElement("button");
+var btnLink    = document.createElement("button");
+var btnCv      = document.createElement("button");
+var btnRetour  = document.createElement("button");
+
+lblAjout.textContent = "Sélectionner un élément à ajouter :";
+
+btnTxt.setAttribute("class", "button");
+btnTxt.setAttribute("id", "btn-txt");
+btnTxt.setAttribute("type", "button");
+btnTxt.innerHTML = "Texte";
+
+btnImg.setAttribute("class", "button");
+btnImg.setAttribute("id", "btn-img");
+btnImg.setAttribute("type", "button");
+btnImg.innerHTML = "Image";
+
+btnLink.setAttribute("class", "button");
+btnLink.setAttribute("id", "btn-link");
+btnLink.setAttribute("type", "button");
+btnLink.innerHTML = "Lien";
+
+btnCv.setAttribute("class", "button");
+btnCv.setAttribute("id", "btn-cv");
+btnCv.setAttribute("type", "button");
+btnCv.innerHTML = "CV";
+
+btnRetour.setAttribute("class", "button");
+btnRetour.setAttribute("id", "btn-back");
+btnRetour.setAttribute("type", "button");
+btnRetour.innerHTML = "Retour";
+
+function modifTools()
+{
+
+    var divBtnSelect  = document.getElementById("btnselect");
+    var divBottom     = document.getElementById("bottom");
+
+    while (divBtnSelect.firstChild)
+    {
+        divBtnSelect.removeChild(divBtnSelect.firstChild);
+    }
+
+    divBottom.removeChild(divBottom.firstChild);
+
+
+    divBtnSelect.appendChild(lblAjout);
+    divBtnSelect.appendChild(btnTxt);
+    divBtnSelect.appendChild(btnImg);
+    divBtnSelect.appendChild(btnLink);
+    divBtnSelect.appendChild(btnCv);
+
+    divBottom.appendChild(btnRetour);
+
+    btnRetour.addEventListener("click", toolsBase, false);
+
+
+}
+
+window.addEventListener("load", toolsBase, false);
