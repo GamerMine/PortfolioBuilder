317fdf0b2554c61ee4a6452e0b8f08c8ac2e11ac
0nyxis <0nyxispro.contact@gmail.com>
2023-04-04 15:22:45 +0200

    Added a test functio to add elements to HTML page

--- js/elements/utils.js
+++ js/elements/utils.js
@@ -60,3 +60,24 @@ export function jsonToHTML(json_content, container) {
         }
     }
 }
+
+export function testJson() 
+{
+    let page = new HTMLPage();
+
+    let titre = new Title("patate",2);
+    let para = new Paragraph("Bonjour, ceci est un test de paragraphe");
+    let link = new Link("on fait des tests","au.cas.ou");
+    let image = new Picture("./images.jpg","elle marche pô");
+    let button = new Button("JE SUIS UN BOUTON CLIQUEZ-MOI");
+    let pdf = new PDFView("https://pdf1.alldatasheet.fr/pdfjsview/web/viewer.html?file=//pdf1.alldatasheet.fr/datasheet-pdf/view/25575/STMICROELECTRONICS/ULN2003/+Q4WJ7UORlHDyRHOIpa/1XXyxeoia+IHpM+/datasheet.pdf");
+
+    page.addObject = link ;
+    page.addObject = para;
+    page.addObject = titre;
+    page.addObject = image;
+    page.addObject = button;
+    page.addObject = pdf;
+
+    return JSON.stringify(page);
+}
