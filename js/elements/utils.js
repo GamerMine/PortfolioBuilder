a874348465c6b895ff59a0ea54982a06ac853801
sm211563 <maxime-sav@outlook.fr>
2023-04-05 09:40:38 +0200

    HTMLObject file

--- js/elements/utils.js
+++ js/elements/utils.js
@@ -1,16 +1,3 @@
-export class HTMLObject{
-    constructor(id) {
-        this.identifier = id;         // De type String
-        this.properties = [];     // De type String
-        this.styleList = [] ;     // De type Style
-    }
-
-    get getIdentifier() { return this.identifier; }
-    get getProperties() { return this.properties; }
-    get getStyleList() { return this.styleList; }
-    set addStyleStyle(style) { this.styleList.push(style); }
-}
-
 export function jsonToHTML(json_content, container) {
 
     const parse_json = JSON.parse(json_content).objectList;
