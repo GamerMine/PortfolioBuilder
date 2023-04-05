e5f36d07728380507de5d625fda9ad0c20eb49d4
leo.langlois <langlois76190@gmail.com>
2023-04-05 17:55:48 +0200

    settings et lien et CV

--- js/settings.js
+++ js/settings.js
@@ -1,8 +1,46 @@
 
 
-document.getElementById("img-settings").onclick = toolsBase;
 
-function settingsBase()
+document.getElementById("img-settings").onclick = settingsGeneraux;
+
+
+
+let lblStyleG = document.createElement("div");
+lblStyleG.innerHTML = "Style Général";
+
+
+function settingsGeneraux()
 {
+    let divSelect  = document.getElementById("btnselect");
+    let divBottom  = document.getElementById("bottom");
 
-}
+    while (divSelect.firstChild)
+    {
+        divSelect.removeChild(divSelect.firstChild);
+    }
+
+    while (divBottom.firstChild)
+    {
+        divBottom.removeChild(divBottom.firstChild);
+    }
+
+    let sepa = document.createElement("hr");
+
+    let btnQuitter = document.createElement("button");
+
+    btnQuitter.setAttribute("class", "button");
+    btnQuitter.setAttribute("id", "btn-quitter");
+    btnQuitter.setAttribute("type", "button");   
+    btnQuitter.innerHTML = "Quitter"; 
+
+
+    divSelect.appendChild(lblStyleG);
+    divSelect.appendChild(sepa);
+    divSelect.appendChild(btnQuitter);
+
+    btnQuitter.addEventListener('click', (event) => 
+    {
+        window.location.href = "../index.html";
+    });
+
+}   
